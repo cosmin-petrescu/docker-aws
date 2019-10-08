9a83b1dc2a809e0824291111569235cc9bd0de0f
Cosmin Petrescu <cosmin.petrescu@innobyte.com>
2019-10-08 11:13:24 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 docker build -t docker-aws .
-docker tag backend:latest cosminpetrescu/docker-aws:latest
+docker tag docker-aws:latest cosminpetrescu/docker-aws:latest
 docker push cosminpetrescu/docker-aws:latest
